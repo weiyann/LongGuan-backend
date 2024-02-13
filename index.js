918f38ddee347a60785844b2b1cc74bb7418d4a3
weiyann <jona11105@gmail.com>
2024-02-13 14:14:31 +0800

    改成獲得資料的韓式，輸出output

--- index.js
+++ index.js
@@ -18,11 +18,52 @@ app.get("/", (req, res) => {
   res.send("<h2>abc</h2>");
 });
 
+//  獲得客人資料的函式
+const getGuestList = async (req) => {
+  const perPage = 10; // 每頁20筆
+  let page = +req.query.page || 1; // 用戶決定要看第幾頁
+
+  let where = `WHERE 1 `; // 1後面要有空白
+
+  let totalRows = 0;
+  let totalPages = 0;
+  let rows = [];
+
+  // 輸出
+  let output = {
+    success: false,
+    page,
+    perPage,
+    rows,
+    totalRows,
+    totalPages,
+    // redirect: "",
+    // info: "",
+  };
+
+  const t_sql = `SELECT COUNT(1) totalRows FROM guest `;
+  [[{ totalRows }]] = await db.query(t_sql);
+  totalPages = Math.ceil(totalRows / perPage);
+
+  if (totalRows > 0) {
+    // if (page > totalPages) {
+    //   output.redirect = `?page=${totalPages}`;
+    //   output.info = `頁碼值大於總頁數`;
+    //   return { ...output, totalRows, totalPages };
+    // }
+    const sql = `SELECT * FROM guest ORDER BY guest_id desc LIMIT ${
+      (page - 1) * perPage
+    },${perPage}`;
+    [rows] = await db.query(sql);
+    output = { ...output, success: true, rows, totalRows, totalPages };
+  }
+  return output;
+};
+
 // 獲得客人資料
 app.get("/guest-list", async (req, res) => {
-  const sql = `SELECT * FROM guest ORDER BY guest_id desc`;
-  const [rows] = await db.query(sql);
-  res.json(rows);
+  const output = await getGuestList(req);
+  res.json(output);
 });
 
 // 新增客人資料

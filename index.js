509a596f13b8754e61dc28b5ad6a277e872b698d
weiyann <jona11105@gmail.com>
2024-02-13 17:01:22 +0800

    keyword search

--- index.js
+++ index.js
@@ -20,10 +20,22 @@ app.get("/", (req, res) => {
 
 //  獲得客人資料的函式
 const getGuestList = async (req) => {
-  const perPage = 10; // 每頁20筆
+  const perPage = 15; // 每頁20筆
   let page = +req.query.page || 1; // 用戶決定要看第幾頁
 
+  let keyword =
+    req.query.keyword && typeof req.query.keyword === "string"
+      ? req.query.keyword.trim()
+      : "";
+  let keyword_ = db.escape(`%${keyword}%`); // 跳脫
+  let qs = {};
+
   let where = `WHERE 1 `; // 1後面要有空白
+  if (keyword) {
+    // 如果有提供關鍵字，將其加入 qs 物件中，以便後續在模板中使用
+    qs.keyword = keyword;
+    where += `AND(\`guest_name\`LIKE${keyword_} OR \`company_name\`LIKE${keyword_} OR \`national_id\`LIKE${keyword_} OR \`phone\`LIKE${keyword_}) `;
+  }
 
   let totalRows = 0;
   let totalPages = 0;
@@ -37,21 +49,28 @@ const getGuestList = async (req) => {
     rows,
     totalRows,
     totalPages,
+    qs,
     // redirect: "",
     // info: "",
   };
+  // 如果頁碼小於1,導向第一頁
+  if (page < 1) {
+    output.redirect = `?page=1`;
+    output.info = `頁碼值小於1`;
+    return output;
+  }
 
-  const t_sql = `SELECT COUNT(1) totalRows FROM guest `;
+  const t_sql = `SELECT COUNT(1) totalRows FROM guest ${where}`;
   [[{ totalRows }]] = await db.query(t_sql);
   totalPages = Math.ceil(totalRows / perPage);
 
   if (totalRows > 0) {
-    // if (page > totalPages) {
-    //   output.redirect = `?page=${totalPages}`;
-    //   output.info = `頁碼值大於總頁數`;
-    //   return { ...output, totalRows, totalPages };
-    // }
-    const sql = `SELECT * FROM guest ORDER BY guest_id desc LIMIT ${
+    if (page > totalPages) {
+      output.redirect = `?page=${totalPages}`;
+      output.info = `頁碼值大於總頁數`;
+      return { ...output, totalRows, totalPages };
+    }
+    const sql = `SELECT * FROM guest ${where} ORDER BY guest_id desc LIMIT ${
       (page - 1) * perPage
     },${perPage}`;
     [rows] = await db.query(sql);
